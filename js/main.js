2b629de8314b57ac4965cbf9988de7929d8275c1
Muhammad <m.ihsanaziz1@gmail.com>
2022-06-04 01:44:23 -0600

    Used HTML templates to cards

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 //POKEDEX
 
+//DOCUMENT ELEMENTS
+const pokemonTemplate = document.querySelector("[data-pokemon-template]");
+const cardsContainer = document.getElementById("flex-container");
+
 //GET JSON DATA
 datafromURL("https://m-aziz1.github.io/Pokedex/pokedex.json");
 
@@ -7,42 +11,19 @@ function datafromURL(address) {
   fetch(address)
     .then((rawData) => rawData.json())
     .then((data) => {
-      let pokemon = Object.entries(data);
-      createCards(pokemon);
+      let numbers = paddedNumberList(data, 3);
+
+      for (let i = 0; i < data.length; i++) {
+        const card = pokemonTemplate.content.cloneNode(true).children[0];
+        const image = card.querySelector("[data-image]");
+        const info = card.querySelector("[data-info]");
+        image.src = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${numbers[i]}.png`;
+        image.alt = `${numbers[i]}`;
+        info.innerHTML = `<p>${numbers[i]}<br>${data[i]["name"]["english"]}<br/>${data[i]["type"].join(", ")}</p>`;
+        card.addEventListener("click", () => alert(data[i]["name"]["english"]));
+        cardsContainer.appendChild(card);
+      }
     });
-}
-
-//INFO CARDS GENERATOR
-const cardsBody = document.getElementById("flex-container");
-
-function createCards(dataArray) {
-  let numbers = paddedNumberList(dataArray, 3);
-
-  for (let i = 0; i < dataArray.length; i++) {
-    const card = document.createElement("div");
-    card.classList.add("card-container");
-
-    const img = Object.assign(document.createElement("img"), {
-      src: `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${numbers[i]}.png`,
-      alt: `${numbers[i]}`,
-    });
-
-    //create text block
-    const textDiv = Object.assign(document.createElement("div"), {
-      classList: "card-info",
-    });
-    const text = Object.assign(document.createElement("p"), {
-      innerHTML: `<p>${numbers[i]}<br>${
-        dataArray[i][1]["name"]["english"]
-      }<br/>${dataArray[i][1]["type"].join(", ")}</p>`,
-    });
-    textDiv.appendChild(text);
-
-    card.append(img, textDiv);
-    cardsBody.appendChild(card);
-
-    card.addEventListener("click", () => alert(dataArray[i][1]["name"]["english"]));
-  }
 }
 
 //Pad Numbers
@@ -62,7 +43,7 @@ function paddedNumberList(dataArray, placeValues) {
 
 //Search Bar
 const searchInput = document.getElementById("input-bar");
-searchInput.addEventListener("input", text => {
+searchInput.addEventListener("input", (text) => {
   const value = text.target.value;
   console.log(value);
-})
+});
